ea6b9d9735fff24b275576b408283e166b062576
onepierre <clarou.p@free.fr>
2021-07-23 21:12:46 +0200

    added gradient clipping

--- main.py
+++ main.py
@@ -34,9 +34,11 @@ def validation_score(model,tokenizer,test_loader_full):
   juste = 0
   out = []
   targets = []
+  loss_sum = []
   n = len(test_loader_full)
   #test_loader = [test_loader_full[i] for i in np.random.choice(n, 400)]
   test_loader = test_loader_full
+  
   nb_chunks_val = ceil(len(test_loader)/20)
   chunk = chunks(test_loader,20)
   print("Test val")
@@ -44,6 +46,10 @@ def validation_score(model,tokenizer,test_loader_full):
   count = 0
   t0_test = time.time()
   for batch in chunk: ## If you have a DataLoader()  object to get the data. 
+    target_temp = []
+    for i in [id for _,id in batch]:
+      target_temp.append(i)
+      targets.append(i)
     count += 1 
     t1_test = time.time()
     t_done_test = t1_test-t0_test
@@ -52,25 +58,20 @@ def validation_score(model,tokenizer,test_loader_full):
 
     with torch.no_grad():  
       encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,max_length=512)
-      outputs = model(**encoding)
+      loss, outputs = model(**encoding,labels = torch.tensor(target_temp))
       outputs = F.softmax(outputs, dim=1)
-      
-      
+      loss_sum.append(loss.item())
     for i in outputs:
       out.append(i.numpy())
-    for i in [id for _,id in batch]:
-      targets.append(i)
 
 
-  loss_fct = nn.CrossEntropyLoss()
-  loss = loss_fct(torch.FloatTensor(out), torch.LongTensor(targets))
   total = 0
   for i,a in enumerate(out):
     total+=1
     if a.argmax() == targets[i]:
       juste += 1
-  print("Accuracy:" + str(juste/total))
-  print("Loss:" + str(loss.item()))
+  print("Accuracy: " + str(juste/total))
+  print("Test Loss: " + str(np.mean(np.array(loss_sum))))
 
 """# Model"""
 class CustomBERTModel(nn.Module):
@@ -84,9 +85,11 @@ class CustomBERTModel(nn.Module):
           #     param.requires_grad = False
           ### New layers:
           self.dropout = nn.Dropout(0.1)
-          self.linear1 = nn.Linear(768, 3)
-          #self.linear2 = nn.Linear(256, 3) ## 3 is the number of classes in this example
+          self.classifier = nn.Linear(768, 256)
+
+          self.classifier2 = nn.Linear(256, 3) ## 3 is the number of classes in this example
           """Initialize the weights"""
+
           for module in self.children():
             if isinstance(module, nn.Linear):
                 module.weight.data.normal_(mean=0.0, std=0.02)
@@ -100,7 +103,6 @@ class CustomBERTModel(nn.Module):
                 module.bias.data.zero_()
                 module.weight.data.fill_(1.0)
 
-
     def forward(
         self,
         input_ids=None,
@@ -112,8 +114,11 @@ class CustomBERTModel(nn.Module):
         output_attentions=None,
         output_hidden_states=None,
         return_dict=None,
+        labels = None
     ):
-          outputs = self.bert(
+
+
+        outputs = self.bert(
             input_ids,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
@@ -124,17 +129,22 @@ class CustomBERTModel(nn.Module):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
-          
-          # sequence_output has the following shape: (batch_size, sequence_length, 512)
-          pooled_output = self.dropout(outputs[1])
-          linear1 = self.linear1(pooled_output)
-          # linear1 = F.relu(linear1)
-          # logits = self.linear2(linear1)
-          #linear1_output = self.linear1(sequence_output[:,0,:].view(-1,768)) ## extract the 1st token's embeddings
-          #linear2_output = self.linear2(linear1_output)
 
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        temp_output = F.relu(self.classifier(pooled_output))
+        
+        logits = self.classifier2(temp_output)
 
-          return linear1
+        loss = None
+
+        if labels is not None:      
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, 3), labels.view(-1))
+
+        output = (logits,) + outputs[2:]
+        return ((loss,) + output) if loss is not None else output
+
 
 
 if __name__ == "__main__":
@@ -155,19 +165,16 @@ if __name__ == "__main__":
 
   """# Train"""
 
-  device = torch.device("cpu")
+
 
   model = CustomBERTModel() # You can pass the parameters if required to have more flexible model
-  #model.to(torch.device(device)) ## can be gpu
-  criterion = nn.CrossEntropyLoss() ## If required define your own criterion
-
-  optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()),lr=5e-5)
-  #optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+  
+  #device = torch.device("cpu")
+  #model.to(device) ## if gpu
 
 
   decay_parameters = get_parameter_names(model, [nn.LayerNorm])
   decay_parameters = [name for name in decay_parameters if "bias" not in name]
-  print(len(decay_parameters))
   # self.args.weight_decay
 
   optimizer_grouped_parameters = [
@@ -184,7 +191,7 @@ if __name__ == "__main__":
   optimizer_kwargs = {
       "betas":  (0.9, 0.999),
       "eps": 1e-8,
-      "lr": 5e-5,
+      "lr": 5e-05,
   }
 
   # torch.optim.lr_scheduler.LambdaLR()
@@ -201,11 +208,11 @@ if __name__ == "__main__":
 
   optimizer = AdamW(optimizer_grouped_parameters, **optimizer_kwargs)
 
-  scheduler = get_scheduler(
+  lr_scheduler = get_scheduler(
                   "linear",
                   optimizer,
                   num_warmup_steps=500,
-                  num_training_steps=nb_chunks*3,
+                  num_training_steps=nb_chunks,
               )
 
 
@@ -219,8 +226,9 @@ if __name__ == "__main__":
   t0 = time.time()
   for epoch in epochs:
       counter = 0
-      
+      count_loss = 0
       for batch in chunks(data_loader,8): ## If you have a DataLoader()  object to get the data.
+          
           
           #model.zero_grad() 
           counter += 1
@@ -228,29 +236,27 @@ if __name__ == "__main__":
           t_done = t1-t0
           printProgressBar(counter, nb_chunks, prefix = 'Progress:', suffix = 'Complete', length = 50,t_done = t_done)
           if counter%100==0:
+              
               model.eval()
               print(str( 8*counter) + "/" + str(len(data_loader)))
-              print("optimizer")
-              print(optimizer)
               validation_score(model,tokenizer,test_loader)
-              
+              print("Train Loss: " + str(count_loss/100))
+              count_loss = 0
+          data = [sent for sent,id in batch]
+          targets = torch.tensor([id for sent,id in batch])## assuming that data loader returns a tuple of data and its targets
 
+          encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True, max_length=512)
 
           model.train()
           optimizer.zero_grad() 
           
-          data = [sent for sent,id in batch]
-          targets = torch.tensor([id for sent,id in batch])## assuming that data loader returns a tuple of data and its targets
-
-          encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,max_length=512)
-
-          
-          outputs = model(**encoding)
+          loss, outputs = model(**encoding,labels = targets)
+          nn.utils.clip_grad_norm_(model.parameters(),1.0,)
           outputs = F.softmax(outputs, dim=1)
-          
 
-          loss = criterion(outputs, targets) 
+
           loss.backward()
+          count_loss += loss.item()
           optimizer.step()
-          scheduler.step()
+          lr_scheduler.step()
 
