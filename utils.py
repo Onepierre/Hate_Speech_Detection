7b1ba077cafbcdc945cb85c2bd202b1b8769ec0b
onepierre <clarou.p@free.fr>
2021-07-24 17:21:07 +0200

    utils

--- utils.py
+++ utils.py
@@ -47,3 +47,64 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+def convert_dataset_to_list(df):
+  sentences_list = []
+  labels_list = []
+  for i in df:
+    text = ""
+    for word in i['post_tokens']:
+      text += " "
+      text+= word
+    sentences_list.append(text)  
+    labels = i['annotators']["label"]
+    value = np.argmax(np.bincount(labels))
+    labels_list.append(value)
+  return sentences_list,labels_list
+
+def validation_score(model,tokenizer,test_loader,data_loader,counter,count_loss,batch_valid):
+
+
+  print(str( 8*counter) + "/" + str(len(data_loader)))
+
+  model.eval()
+  count = 0
+  juste = 0
+  out = []
+  targets = []
+  loss_sum = []
+  
+  nb_chunks_val = ceil(len(test_loader)/20)
+  chunk = chunks(test_loader,20)
+
+  print("Test val")
+  printProgressBar(0, nb_chunks_val, prefix = 'Progress:', suffix = 'Complete', length = 50)
+  count = 0
+  t0_test = time.time()
+  for batch in chunk:
+    target_temp = []
+    for i in [id for _,id in batch]:
+      target_temp.append(i)
+      targets.append(i)
+    count += 1 
+    t1_test = time.time()
+    t_done_test = t1_test-t0_test
+    printProgressBar(count, nb_chunks_val, prefix = 'Progress:', suffix = 'Complete', length = 50, t_done=t_done_test)  
+    data = [sent for sent,id in batch]
+
+    with torch.no_grad():  
+      encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,max_length=512)
+      loss, outputs = model(**encoding,labels = torch.tensor(target_temp))
+      outputs = F.softmax(outputs, dim=1)
+      loss_sum.append(loss.item())
+    for i in outputs:
+      out.append(i.numpy())
+
+  for i,a in enumerate(out):
+    if a.argmax() == targets[i]:
+      juste += 1
+  print("Accuracy: " + str(juste/len(out)))
+  print("Test Loss: " + str(np.mean(np.array(loss_sum))))
+  print("Train Loss: " + str(count_loss/batch_valid))
+
+
+
