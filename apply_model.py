c656961e736fe40cf6d1c82a13519cf0a1934934
onepierre <clarou.p@free.fr>
2021-08-04 19:12:24 +0200

    french and english models

--- apply_model.py
+++ apply_model.py
@@ -1,11 +1,12 @@
 from logging import raiseExceptions
+import pandas as pd
 from transformers import BertTokenizerFast,RobertaTokenizerFast
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from math import ceil
 from utils import *
-from model.model import CustomBERTModel,CustomRoBERTaModel,CustomCamemBERTModel
+from model.model import CustomBERTModel,CustomRoBERTaModel,CustomCamemBERTModel,MultiLabelCamemBERTModel
 from load_fr_dataset import loader
 
 
@@ -46,7 +47,7 @@ def evaluate(test_loader):
             juste += 1
     print("Accuracy: " + str(juste/len(out)))
 
-def predict(sentences):
+def predict(model,tokenizer,sentences):
     # Predict labels of sentences 
     # sentences is a list of sentences
     model.eval()
@@ -71,7 +72,7 @@ def predict(sentences):
                 logits.append(i.numpy())
     return logits
 
-if __name__ == "__main__":
+def threeLabelsFR():
     model_to_test = "camembert"
     labels = ["Normal", "Offensive", "Hateful"]
     if model_to_test == "camembert":
@@ -83,6 +84,8 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load("model_save/camembert.ckpt"))
         model.eval()
 
+
+
         _,test_loader = loader()
         # evaluate(test_loader)
         sentences = []
@@ -91,10 +94,45 @@ if __name__ == "__main__":
 
         #sentences = ["Je deteste les arabes","Je deteste les noirs","Je deteste les chinois","Je deteste les blancs","Les arabes sont dangereux","J'aime pas les sales noirs","je suis arabe","j'aime le chocolat", "ce mec est un turbo débile","tu es un colossal étron","tu es génial", "les africains sont intelligents", "les africains sont méchants", "les africains sont nuls"]
         sentences = ["Et là, il suffit de dériver","On utilise le théorème de Pythagore","On dit Lazos, pas Lazaux"]
-        logits = predict(sentences)
+        logits = predict(model,tokenizer,sentences)
         print(logits)
         for i in range(len(logits)):
             id = np.argmax(np.array(logits[i]))
             print(sentences[i])
             print(logits[i])
             print(labels[id])
+            
+def threeLabelsEN():
+    model_to_test = "bert-base-uncased"
+    labels = ["Hateful", "Normal", "Offensive"]
+    if model_to_test == "bert-base-uncased":
+        # TESTING
+        model_name = "roberta-base"
+
+        tokenizer = RobertaTokenizerFast.from_pretrained(model_name, do_lower_case=True)
+        model = CustomRoBERTaModel(bert_name ="roberta-base") 
+        model.load_state_dict(torch.load("model_save/RoBERTa(768-3).ckpt"))
+        model.eval()
+        print("loaded")
+
+        sentences = ["You stupid!", "I ate a pizza","You son of a bitch","Black people are dumb","mulsim should leave our country, they belong to jail","You are useless","You're the most useless shit I've ever seen.", "What's up bitches", "Shut up you nigger","Hello, nigga"]
+        logits = predict(model,tokenizer,sentences)
+        for i in range(len(logits)):
+            id = np.argmax(np.array(logits[i]))
+            print(sentences[i])
+            print(logits[i])
+            print(labels[id])
+
+def read_data():
+    # 0 Normal, 1 Offensive, 2 Hate
+    data = pd.read_csv("datasets/test_en.csv",index_col = 0)
+    data = data[['class','tweet']]
+    for i in range(len(data)):
+        data.at[i,"tweet"] = data.iloc[i]["tweet"].replace("!","")
+    res = [(line["tweet"],line["class"]) for _,line in data.iterrows()]
+    print(res)
+    return res 
+
+if __name__ == "__main__":
+    #read_data()
+    threeLabelsEN()
